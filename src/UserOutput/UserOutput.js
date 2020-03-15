9ddf77ab268b437c5cf702d2b8e6c243bad54563
Igor Skliarsky <shkliarskiyigor@gmail.com>
2020-03-15 23:40:42 +0100

    first commit

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './UserOutput.css'
+
+const userOutput = (props) => {
+    return (
+        <div className="UserOutput">
+            <p>i'm person and i'm  years old</p>
+            <p>i'm person and i'm  years old</p>
+        </div>
+    )
+}
+
+export default userOutput;
